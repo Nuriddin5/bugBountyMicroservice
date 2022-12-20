07461a61dc9ffc8938096e5094b4619a0f781e00
Nuriddin Qurbonboyev <nuriddinqurbonboyev@mail.ru>
2022-12-20 16:29:51 +0500

    Bismillah

---
+++ src/main/java/com/nuriddin/bugbountymicroservice/BugBountyMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.nuriddin.bugbountymicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugBountyMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BugBountyMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nuriddin/bugbountymicroservice/BugBountyMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuriddin.bugbountymicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugBountyMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
